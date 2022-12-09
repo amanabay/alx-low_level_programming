9a681e5f2b68426866f2918195310eab69e88913
amanabay <amanuelabay22@gmail.com>
2022-12-08 17:56:00 -0800

    4-puts.c betty

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,12 @@
 #include <stdio.h>
 
+/*
+ * main - Entry point
+ *
+ * prints str using puts function
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	char str[] = "\"Programming is like building a multilingual puzzle";
