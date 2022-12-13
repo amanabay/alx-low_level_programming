268391948f9ead43f0772176cc76ba28caa25d3f
amanabay <amanuelabay22@gmail.com>
2022-12-13 14:20:12 -0800

    Added 9-times_table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * times_table - prints the 9 times table, starting with 0.
+ */
+void times_table(void)
+{
+	int rows, cols, product;
+
+	for (rows = 0; rows < 10; rows++)
+	{
+		_putchar('0');
+		for (cols = 0; cols < 10; cols++)
+		{
+			_putchar(',');
+			_putchar(' ');
+			product = rows * cols;
+			if (product <= 9)
+				_putchar(' ');
+			else
+				_putchar((product / 10) + '0');
+			_putchar((product % 10) + '0');
+			if (cols == 9)
+				break;
+		}
+		_putchar('\n');
+	}
+}
