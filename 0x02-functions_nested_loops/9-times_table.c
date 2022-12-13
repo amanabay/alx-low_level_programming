5c6942459286ae08649b1c80c1e0b586a3976290
amanabay <amanuelabay22@gmail.com>
2022-12-13 14:30:37 -0800

    Removed spaces from first column

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -9,18 +9,17 @@ void times_table(void)
 
 	for (rows = 0; rows < 10; rows++)
 	{
+		_putchar('0');
 		for (cols = 0; cols < 10; cols++)
 		{
+			_putchar(',');
+			_putchar(' ');
 			product = rows * cols;
 			if (product <= 9)
 				_putchar(' ');
 			else
 				_putchar((product / 10) + '0');
 			_putchar((product % 10) + '0');
-			if (cols == 9)
-				break;
-			_putchar(',');
-			_putchar(' ');
 		}
 		_putchar('\n');
 	}
