37209b55cd606ba039ba4bb912e99ba43972d17f
amanabay <amanuelabay22@gmail.com>
2022-12-13 16:56:49 -0800

    fibonacci madness

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - prints the first 50 Fibonacci numbers, starting
+ *	with 1 and 2, followed by a new line.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int m, n, o, i;
+
+	m = 1;
+	n = 2;
+	for (i = 0; i < 50; i++)
+	{
+		printf("%d", m);
+		if (i == 49)
+			break;
+		printf(", ");
+		o = m + n;
+		m = n;
+		n = o;
+	}
+	printf("\n");
+	return (0);
+}
