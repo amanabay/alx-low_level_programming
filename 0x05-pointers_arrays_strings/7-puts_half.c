f99256ddeaf9ca47b6ae5a72c1816c35b873b55c
amanabay <amanuelabay22@gmail.com>
2022-12-19 08:09:14 -0800

    speed is not always the answer

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * puts_half -  prints half of a string, followed by a new line. 
+ * @str: string to be printed.
+ */
+void puts_half(char *str)
+{
+	int len = 0, i = 0;
+
+	while (str[i++])
+		len++;
+	for (i = len / 2; i < len; i++)
+		_putchar(str[i]);
+	_putchar('\n');
+}
