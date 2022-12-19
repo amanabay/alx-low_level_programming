4f9f74548eaa38da09a490e269e85475d473b2c5
amanabay <amanuelabay22@gmail.com>
2022-12-19 08:49:33 -0800

    adding null pointer to end

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,8 +1,8 @@
 #include "main.h"
 
 /**
- * *_strcpy - copies the string pointed to by src, including 
- * the terminating null byte (\0), to the buffer 
+ * *_strcpy - copies the string pointed to by src, including
+ * the terminating null byte (\0), to the buffer
  * pointed to by dest.
  * @dest: destination string
  * @src: source string
@@ -10,10 +10,13 @@
  */
 char *_strcpy(char *dest, char *src)
 {
-	int i;
+	int len = 0, i = 0;
 
-	for (i = 0; src[i] != '\0'; i++)
+	while (src[i++])
+		len++;
+	for (i = 0; i < len; i++)
 		dest[i] = src[i];
+	dest[len] = '\0';
 	return (dest);
 
 }
