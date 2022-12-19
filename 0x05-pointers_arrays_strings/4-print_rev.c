22e7d1f04f154de27755768d2d44967a11690e92
amanabay <amanuelabay22@gmail.com>
2022-12-19 05:30:17 -0800

    Seg faults all over

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_rev -  prints a string, in reverse, followed by a new line.
+ *
+ * @s: string to be printed in reverse
+ */
+void print_rev(char *s)
+{
+	int len, i;
+
+	len = 0;
+	while (s[i++])
+		len++;
+	for (i = len - 1; i >= 0; i--)
+		_putchar(s[i]);
+	_putchar('\n');
+}
