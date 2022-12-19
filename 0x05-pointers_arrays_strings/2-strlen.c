36b36faa79e4b4a1f85a7a9811beac30329ab65c
amanabay <amanuelabay22@gmail.com>
2022-12-19 05:02:46 -0800

    bettying things up

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -2,7 +2,9 @@
 
 /**
  * _strlen - returns the length of a string.
- * @s: string whose length is to be returned.
+ * @s: string whose length is to be returned
+ *
+ * Return: the length of @s
  */
 int _strlen(char *s)
 {
