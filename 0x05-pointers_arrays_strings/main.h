ee273e9357a203146adb4f92f1c76985c6e6e9e9
amanabay <amanuelabay22@gmail.com>
2022-12-18 20:25:32 -0800

    pointers and arrays 0-98

---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,14 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * Desc - File containing prototypes(declarations) for
+ * all functions in the 0x05-pointers_arrays_strings category
+ */
+void reset_to_98(int *n);
+void swap_int(int *a, int *b);
+int _strlen(char *s);
+void _puts(char *str);
+void print_rev(char *s);
+
+#endif
