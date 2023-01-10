6c6094da220a2e61efae7eb26d24c469ca1a55bb
amanabay <amanuelabay22@gmail.com>
2023-01-09 16:45:02 -0800

    malloc return

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -16,7 +16,8 @@ char *create_array(unsigned int size, char c)
 
 	if (size == 0)
 		return (NULL);
-	arr = malloc(size * sizeof(char));
+	else
+		arr = malloc(size * sizeof(c));
 	for (i = 0; i < size; i++)
 		arr[i] = c;
 	if (arr == NULL)
