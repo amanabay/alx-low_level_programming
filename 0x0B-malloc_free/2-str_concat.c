46759f8a0b23ddbab681cfd171162c43345848db
amanabay <amanuelabay22@gmail.com>
2023-01-09 17:33:32 -0800

    malloc size issue fix2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -22,7 +22,7 @@ char *str_concat(char *s1, char *s2)
 		len1++;
 	while (s2[len2] != '\0')
 		len2++;
-	clen = len1 + len2;
+	clen = len1 + len2 + 1;
 	cs = malloc(clen * sizeof(char));
 	if (cs == NULL)
 		return (NULL);
