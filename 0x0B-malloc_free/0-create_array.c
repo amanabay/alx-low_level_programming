e6f8b872595547ad21844501881862456df91348
amanabay <amanuelabay22@gmail.com>
2023-01-09 16:49:42 -0800

    fixing malloc return issue

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -16,11 +16,10 @@ char *create_array(unsigned int size, char c)
 
 	if (size == 0)
 		return (NULL);
-	else
-		arr = malloc(size * sizeof(c));
-	for (i = 0; i < size; i++)
-		arr[i] = c;
+	arr = malloc(size * sizeof(c));
 	if (arr == NULL)
 		return (NULL);
+	for (i = 0; i < size; i++)
+		arr[i] = c;
 	return (arr);
 }
