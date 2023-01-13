8ffd4bfa51ca5fbb6f3df137ae344c79fd63e30a
amanabay <amanuelabay22@gmail.com>
2023-01-12 18:45:13 -0800

    1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -27,9 +27,9 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	cs = malloc((len1 + n + 1) * sizeof(char));
 	if (cs == NULL)
 		return (NULL);
-	for (i = 0; i < (len1 + n + 1) && s1[i] != '\0'; i++)
+	for (i = 0; i < len1; i++)
 		cs[i] = s1[i];
-	for (j = 0; ; i < (len1 + n + 1) && s2[j] != '\0'; j++)
+	for (j = 0; j <= n; j++)
 	{
 		cs[i] = s2[j];
 		i++;
