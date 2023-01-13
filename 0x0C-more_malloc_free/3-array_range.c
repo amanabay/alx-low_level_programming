d2018d9915a0312a86c16bee8935fd309dac8abc
amanabay <amanuelabay22@gmail.com>
2023-01-12 18:40:47 -0800

    tiny mistakes

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -16,6 +16,8 @@ int *array_range(int min, int max)
 		return (NULL);
 	size = max - min + 1;
 	arr = malloc(size * sizeof(int));
+	if (arr == NULL)
+		return (NULL);
 	for (i = 0; i < size; i++)
 		arr[i] = min++;
 	return (arr);
