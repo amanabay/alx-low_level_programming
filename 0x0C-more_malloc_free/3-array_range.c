a06c5b999ecdbdd525f265d399d67e7f61ab2dc8
amanabay <amanuelabay22@gmail.com>
2023-01-12 18:38:17 -0800

    added size var

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -10,12 +10,13 @@
  */
 int *array_range(int min, int max)
 {
-	int *arr, i;
+	int *arr, i, size;
 
 	if (min > max)
 		return (NULL);
-	arr = malloc(((max - min) + 1) * sizeof(int));
-	for (i = 0; i < ((max - min) + 1) ; i++)
+	size = max - min + 1;
+	arr = malloc(size * sizeof(int));
+	for (i = 0; i < size; i++)
 		arr[i] = min++;
 	return (arr);
 }
