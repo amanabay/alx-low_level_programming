3b23c04859d0db8ce18975b962bbe28729746865
amanabay <amanuelabay22@gmail.com>
2023-01-16 18:33:35 -0800

    struct puppy

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#ifndef DOG
+#define DOG
+/**
+ * struct dog - struct of a dog datatype
+ * @name: dog's name
+ * @age: age of the dog
+ * @owner: dog's owner
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
