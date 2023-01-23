344c53af95e786df9745db105c26a6ca1502b5f4
amanabay <amanuelabay22@gmail.com>
2023-01-22 16:00:00 -0800

    function pointers-fixed NULL issue

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -3,10 +3,12 @@
 /**
  * print_name - function that prints a name.
  * @name: name to be printed
- * @f: pointer ti function that prints name
+ * @f: pointer to function that prints name
  */
 void print_name(char *name, void (*f)(char *))
 {
+	if (name == NULL || f == NULL)
+		return;
 	f(name);
 }
 
