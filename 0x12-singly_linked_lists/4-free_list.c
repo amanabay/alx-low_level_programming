ec88c7465dfdfdd7c12bc2967db0fc9221a183c0
amanabay <amanuelabay22@gmail.com>
2023-01-26 13:14:55 -0800

    single ll - free list

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * free_list - frees a list_t list.
+ * @head: head of linked list 
+ */
+void free_list(list_t *head)
+{
+	list_t *thead;
+
+	while (head != NULL)
+	{
+		thead = head->next;
+		free(head->str);
+		free(head);
+		head = thead;
+	}
+}
