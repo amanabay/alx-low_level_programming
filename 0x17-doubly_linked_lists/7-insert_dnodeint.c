fa408659721d43fde704875baefe0f1712270b51
amanabay <amanuelabay22@gmail.com>
2023-03-16 20:23:23 +0000

    Fix loop issues

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -24,7 +24,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	if (idx == 0)
 		return (add_dnodeint(h, n));
 
-	for (i = 0; i < (idx - 1); idx++)
+	for (i = 0; i < idx; idx++)
 	{
 		chead = chead->next;
 		if (chead == NULL)
