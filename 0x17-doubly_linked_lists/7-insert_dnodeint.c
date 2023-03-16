346ac4045a1a2387807b7ce4221c70643cfd1397
amanabay <amanuelabay22@gmail.com>
2023-03-16 20:22:05 +0000

    Fix various issues

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -3,38 +3,38 @@
 #include <stdlib.h>
 
 /**
- * insert_dnodeint_at_index - inserts a new node at a given position.
+ * insert_dnodeint_at_index - inserts a node at a given position.
  * @h: head of the linked list
- * @idx: index of the list where the new node should be added
- * @n: data part of new node to be added.
+ * @idx: index of the list where the node should be added
+ * @n: data part of node to be added.
  *
- * Return:  the address of the new node, or NULL if it failed
+ * Return:  the address of the node, or NULL if it failed
  */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	dlistint_t *newnode, *chead;
 	unsigned int i;
+	dlistint_t *newnode;
+	dlistint_t *chead = *h;
 
-	chead = *h;
 	newnode = malloc(sizeof(dlistint_t));
-
 	if (newnode == NULL)
 		return (NULL);
-
+	newnode->n = n;
 
 	if (idx == 0)
 		return (add_dnodeint(h, n));
+
+	for (i = 0; i < (idx - 1); idx++)
+	{
+		chead = chead->next;
+		if (chead == NULL)
+			return (NULL);
+	}
+
 	if (chead->next == NULL)
 		return (add_dnodeint_end(h, n));
 
-	for (i = 0; i < (idx - 1); i++)
-	{
-		if (chead == NULL)
-			return (NULL);
-		chead = chead->next;
-	}
 
-	newnode->n = n;
 	newnode->prev = chead;
 	newnode->next = chead->next;
 	chead->next->prev = newnode;
