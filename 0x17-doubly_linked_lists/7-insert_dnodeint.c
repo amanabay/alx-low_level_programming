49cdc7a7e9961e0f348ecaad09ec27c941381b5f
amanabay <amanuelabay22@gmail.com>
2023-03-16 20:15:28 +0000

    Insert at index betty fix

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -26,7 +26,6 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		return (add_dnodeint(h, n));
 	if (chead->next == NULL)
 		return (add_dnodeint_end(h, n));
-	
 
 	for (i = 0; i < (idx - 1); i++)
 	{
