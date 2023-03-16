f1e61d4d3dc6a105aeccc158c03d77584110b6eb
amanabay <amanuelabay22@gmail.com>
2023-03-16 19:52:25 +0000

    Insert at end

---
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -0,0 +1,46 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * insert_dnodeint_at_index - inserts a new node at a given position.
+ * @head: head of the linked list
+ * @idx: index of the list where the new node should be added
+ * @n: data part of new node to be added.
+ *
+ * Return:  the address of the new node, or NULL if it failed
+ */
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+{
+	dlistint_t *newnode, *chead, *prev;
+	unsigned int i;
+
+	chead = *h;
+	newnode = malloc(sizeof(dlistint_t));
+
+	if (newnode == NULL)
+		return (NULL);
+
+	newnode->n = n;
+
+	if (idx == 0)
+	{
+		newnode->prev = NULL;
+		newnode->next = chead;
+		*h = newnode;
+		return (newnode);
+	}
+
+	for (i = 0; i < (idx - 1); i++)
+	{
+		if (chead == NULL || chead->next == NULL)
+			return (NULL);
+		prev = chead;
+		chead = chead->next;
+	}
+	newnode->prev = prev;
+	newnode->next = chead->next;
+	chead->next = newnode;
+
+	return (newnode);
+}
